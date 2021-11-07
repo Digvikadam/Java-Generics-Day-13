34d24fecd8796ae682c1aacae3a17585578bdfa8
[Digvikadam] <[digvikadam168@gmail.com]>
2021-11-07 10:33:28 +0530

    Added: Print the Max to std out using Generic Method

--- src/com/bridgelabz/CalculateMaxOfThreeValues.java
+++ src/com/bridgelabz/CalculateMaxOfThreeValues.java
@@ -5,21 +5,25 @@ public class CalculateMaxOfThreeValues {
 		Integer[] intArr = {19, 1239, 999, 995};
 		Float[] floatArr = {15.55f, 14.65f, 15.22f, 18.4f};
 		String[] strArr = {"RAMESH" ,"Ramesh" , "ramesh" ,"Rama"};
-
 		//call generic method
 		findMax(intArr);
 		findMax(floatArr);
 		findMax(strArr);
 	}
-	
-	public static <E extends Comparable<E>> void findMax(E[] arr ) {
 
+	//method to find max value
+	public static <E extends Comparable<E>> void findMax(E[] arr ) {
 		E maximum = arr[0];
 		for(int i = 1; i < arr.length ; i++ ) {
 			if(maximum.compareTo(arr[i]) < 0) {
-				maximum = arr[i];
+				maximum = arr[i];            
 			}
 		}
-		System.out.println("maximum is "+maximum);
+		printMax(maximum);
+	}
+
+	//print method to print the obtained max value
+	public static <E> void printMax(E max) {
+		System.out.println("Among the three the maximum is " + max);		
 	}
 }
