c645f6a9e975b04e9216a8bf5a0a6aac303f1edd
[Digvikadam] <[digvikadam168@gmail.com]>
2021-11-07 10:20:20 +0530

    Added: Find Maximum Among Three Float

--- src/com/bridgelabz/CalculateMaxOfThreeValues.java
+++ src/com/bridgelabz/CalculateMaxOfThreeValues.java
@@ -2,11 +2,12 @@ package com.bridgelabz;
 
 public class CalculateMaxOfThreeValues {
 	public static void main(String[] args) {
-		// Three Integer objects initialization
-		Integer firstNumber = 54 ;
-		Integer secondNumber = 34 ;
-		Integer thirdNumber = 21 ;
-		Integer max = firstNumber ;
+		// Three Float objects initialization
+		Float firstNumber = 54.34f ;
+		Float secondNumber = 34.33f ;
+		Float thirdNumber = 54.65f ;
+
+		Float max = firstNumber ;
 
 		// using compareTo method
 		if(secondNumber.compareTo(max) > 0) {
@@ -15,8 +16,7 @@ public class CalculateMaxOfThreeValues {
 		if(thirdNumber.compareTo(max) > 0) {
 			max = thirdNumber ;
 		}
-
-		// printing the maximum among three integer objects
+		// printing the maximum among three Float objects
 		System.out.println("Maximum number is "+max);
 	}
 }
