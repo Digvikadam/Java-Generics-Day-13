5bd97eb150dbf44b5620ecfa48fa14e4609dd11f
[Digvikadam] <[digvikadam168@gmail.com]>
2021-11-07 10:22:31 +0530

    Added: Find Maximum Among Three String

--- src/com/bridgelabz/CalculateMaxOfThreeValues.java
+++ src/com/bridgelabz/CalculateMaxOfThreeValues.java
@@ -2,12 +2,12 @@ package com.bridgelabz;
 
 public class CalculateMaxOfThreeValues {
 	public static void main(String[] args) {
-		// Three Float objects initialization
-		Float firstNumber = 54.34f ;
-		Float secondNumber = 34.33f ;
-		Float thirdNumber = 54.65f ;
+		// Three String objects initialization
+		String firstNumber = "ram";
+		String secondNumber = "ramesh" ;
+		String thirdNumber = "rama" ;
 
-		Float max = firstNumber ;
+		String max = firstNumber ;
 
 		// using compareTo method
 		if(secondNumber.compareTo(max) > 0) {
@@ -16,7 +16,8 @@ public class CalculateMaxOfThreeValues {
 		if(thirdNumber.compareTo(max) > 0) {
 			max = thirdNumber ;
 		}
-		// printing the maximum among three Float objects
-		System.out.println("Maximum number is "+max);
+
+		// printing the maximum among three String objects
+		System.out.println("Maximum string is:"+max);
 	}
 }
